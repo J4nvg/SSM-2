241f3a8bc8d89a141d0485a75240f5172f1558ae
J4nvg_ <janvgestel@outlook.com>
2025-03-10 13:13:26 +0100

    Fixed estEtau i.e. ex 1a,b,c

--- estEtau.py
+++ estEtau.py
@@ -3,7 +3,7 @@ from scipy import stats
 from dist.Distribution import Distribution
 import time
 from statsmodels.stats.weightstats import DescrStatsW
-import line_profiler
+from line_profiler import profile
 
 """
 Previously resulted in incorrect output, yet this was due wronly chosen parameters
@@ -51,7 +51,7 @@ print("95% CI for hitting times:", f"{ci[0]:.3f}", f"{ci[1]:.3f}")
 print(f"Difference from 0.5 = {abs(mean_hitting_time - 0.5):.4f}")
 
 
-
+@profile
 def oneC(dt, mu, sigma,r_start,r_inner,r_outer):
     x = r_start / np.sqrt(2) # Start at top-right as in image
     y = r_start / np.sqrt(2)
@@ -72,35 +72,16 @@ def oneC(dt, mu, sigma,r_start,r_inner,r_outer):
             return time_elapsed,0
 
 
-
-nrRuns = 1000
+# nrRuns = 1e5
+nrRuns = 10
 stepSize = 1e-4
 #Predefined n
 n = 10
+# n = 5
 def r(i):
     radi = 4**(-1) + (4*10)**(i/n)
     return radi
 
-
-# for i_star in range(2, n-1):
-#     inner_times = np.zeros(nrRuns)
-#     outer_times = np.zeros(nrRuns)
-#     for i in range(nrRuns):
-#         tau, boundary = oneC(stepSize, 0, 1, r(i_star), r(i_star - 1), r(i_star + 1))
-#         if boundary == 0:
-#             inner_times[i] = tau
-#         else:
-#             outer_times[i] = tau
-#
-#
-#     Eone = np.mean(inner_times[inner_times < outer_times])
-#     Prob = np.mean(inner_times < outer_times)
-#     Etwo = np.mean(outer_times[outer_times > inner_times])
-#
-#     print(f"i* = {i_star}:")
-#     print("E[τ_inner | τ_inner < τ_outer] =", Eone)
-#     print("P[τ_outer < τ_inner] =", Prob)
-#     print("E[τ_outer | τ_outer < τ_inner] =", Etwo)
 
 for i_star in range(2, n-1):
     print(f"==== [{i_star}] ====")
@@ -112,17 +93,21 @@ for i_star in range(2, n-1):
         hitting_times[i] = tau
         boundaries[i] = boundary
 
-    # Create boolean masks for inner and outer boundary hits.
-    inner_mask = (boundaries == 0)
-    outer_mask = (boundaries == 1)
-
     # Calculate the conditional expectations using the masks.
-    E_inner = np.mean(hitting_times[inner_mask])
-    print("E[τ_inner | τ_inner < τ_outer] =", E_inner)
-
-    E_outer = np.mean(hitting_times[outer_mask])
-    print("E[τ_outer | τ_inner < t_outer] =", E_outer)
+    E_inner = np.mean(hitting_times[boundaries == 0])
+    ci = DescrStatsW(hitting_times[boundaries == 0]).tconfint_mean(alpha=0.05)
+    # print("95% CI for E[τ_inner | τ_inner < τ_outer]:", f"{ci[0]:.3f}", f"{ci[1]:.3f}")
+    print("E[τ_inner | τ_inner < τ_outer] =",f"{E_inner:.3f}", f"±{ci[1]-E_inner:.3f}")
 
     # Calculate the probability of hitting the outer boundary.
-    P_outer = np.mean(outer_mask)  # Booleans are interpreted as 0 (False) or 1 (True)
-    print("P[τ_outer < τ_inner] =", P_outer)
+    P_outer = np.mean(boundaries == 1)
+    ci = DescrStatsW(boundaries == 1).tconfint_mean(alpha=0.05)
+    # print("95% CI for P[τ_outer < τ_inner]:", f"{ci[0]:.3f}", f"{ci[1]:.3f}")
+    print("P[τ_outer < τ_inner] =", f"{P_outer:.3f}", f"±{ci[1]-P_outer:.3f}")
+
+    E_outer = np.mean(hitting_times[boundaries == 1])
+    ci = DescrStatsW(hitting_times[boundaries == 1]).tconfint_mean(alpha=0.05)
+    # print("95% CI for E[τ_outer | τ_inner > τ_outer]:", f"{ci[0]:.3f}", f"{ci[1]:.3f}")
+    print("E[τ_outer | τ_inner > τ_outer] =", f"{E_outer:.3f}", f"±{ci[1]-E_outer:.3f}","\n")
+
+
